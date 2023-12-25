ce095facd51792e4c23b0fb433e419c95ed9c9ef
Oleksandr Perekuta <perekuta.a@gmail.com>
2023-12-25 14:35:06 +0100

    added required changes to use mysql db with flyway migrations

---
+++ Blog/src/main/java/com/blog/user/UserEntity.java
@@ -0,0 +1,24 @@
+package com.blog.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String secondName;
+    private String email;
+    private String password;
+    private boolean isEnabled;
+}
---
+++ Blog/src/main/resources/db/migration/V1__Creation_User.sql
@@ -0,0 +1,8 @@
+create table user(
+    id int auto_increment primary key,
+    first_name varchar(255) not null ,
+    second_name varchar(255) not null ,
+    email varchar(255) not null ,
+    password varchar(255) not null ,
+    is_enabled boolean not null
+)
